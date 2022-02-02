50d295fa57b1407794892ee8cff88b19c68c4f2f
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-02-01 20:39:35 -0600

    created routes for each component card

---
+++ src/ApplicationViews.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./Home"
+import { CustomerCard } from "./components/customer/CustomerCard"
+import { EmployeeCard } from "./components/employee/EmployeeCard"
+import { AnimalList } from "./components/animal/AnimalList"
+
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            <Routes>
+                {/* Render the location list when http://localhost:3000/ */}
+                <Route exact path="/" element={<Home />} />
+
+                {/* Render the animal list when http://localhost:3000/animals */}
+                <Route path="/animals" element={<AnimalList />} />
+                
+                {/* Render the animal list when http://localhost:3000/animals */}
+                <Route path="/customers" element={<CustomerCard />} />
+                
+                {/* Render the animal list when http://localhost:3000/animals */}
+                <Route path="/Employees" element={<EmployeeCard />} />
+            </Routes>
+        </>
+    )
+}
