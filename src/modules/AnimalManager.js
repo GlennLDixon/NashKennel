e19cc5e5116a5b666db01730ea6f0ea03ee794aa
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-02-05 13:51:48 -0600

    add the promise to the end of the fetch on line 8 and created a fetch to delete the animal card

--- src/modules/AnimalManager.js
+++ src/modules/AnimalManager.js
@@ -1,10 +1,15 @@
 const remoteURL = 'http://localhost:8088'
 
 export const getAnimalById = (animalId) => {
-    return fetch(`${remoteURL}/animals/${animalId}?_expand=locationId&_expand=customerId`)
-    .then(res => res.json())
+    return fetch(`${remoteURL}/animals/${animalId}?_expand=locationId&_expand=customerId`).then(res => res.json())
 }
 
 export const getAllAnimals = () => {
-    return fetch(`${remoteURL}/animals`)
+    return fetch(`${remoteURL}/animals`).then(res => res.json())
+} 
+
+export const deleteAnimal = (id) => {
+    return fetch(`${remoteURL}/animals/${id}`, {
+        method: "DELETE"
+    }).then(result => result.json())
 }
