d4b58d3a469a5e13eef9130c4dad0b656bbadadb
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-02-05 18:45:52 -0600

    created a deleteCustomer function that's going to remove each customer card

--- src/modules/CustomerManager.js
+++ src/modules/CustomerManager.js
@@ -3,3 +3,9 @@ const remoteURL = 'http://localhost:8088'
 export const getAllCustomers = () => {
     return fetch(`${remoteURL}/customers`).then(res => res.json())
 } 
+
+export const deleteCustomer = (id) => {
+    return fetch(`${remoteURL}/customers/${id}`, {
+        method: "DELETE"
+    }).then(result => result.json())
+}
