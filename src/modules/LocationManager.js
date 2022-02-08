8c060cc5c3a0801276301bd37315cdecc631cb1d
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-02-07 19:40:57 -0600

    exported a get method for locations by id

--- src/modules/LocationManager.js
+++ src/modules/LocationManager.js
@@ -1,5 +1,9 @@
 const remoteURL = 'http://localhost:8088'
 
+export const getLocationById = (locationId) => {
+    return fetch(`${remoteURL}/locations/${locationId}`).then(res => res.json())
+} 
+
 export const getAllLocations = () => {
     return fetch(`${remoteURL}/locations`).then(res => res.json())
 } 
