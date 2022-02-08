0a7a6177a711bfa9c7dbc5559b372982eea24236
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-02-07 19:47:49 -0600

    created a new route path with the imported AnimalDetail

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -5,6 +5,8 @@ import { CustomerList } from "./components/customer/CustomerList"
 import { EmployeeList } from "./components/employee/EmployeeList"
 import { LocationList } from "./components/location/LocationList"
 import { AnimalList } from "./components/animal/AnimalList"
+import { AnimalDetail } from "./components/animal/AnimalDetail"
+import { LocationDetail } from "./components/location/LocationDetail"
 
 
 export const ApplicationViews = () => {
@@ -16,6 +18,7 @@ export const ApplicationViews = () => {
 
                 {/* Render the animal list when http://localhost:3000/animals */}
                 <Route path="/animals" element={<AnimalList />} />
+                <Route path="/animals/:animalId" element={<AnimalDetail />} />
                 
                 {/* Render the animal list when http://localhost:3000/animals */}
                 <Route path="/customers" element={<CustomerList />} />
@@ -25,6 +28,7 @@ export const ApplicationViews = () => {
                 
                 {/* Render the animal list when http://localhost:3000/animals */}
                 <Route path="/locations" element={<LocationList />} />
+                <Route path="/locations/:locationId" element={<LocationDetail />} />
             </Routes>
         </>
     )
