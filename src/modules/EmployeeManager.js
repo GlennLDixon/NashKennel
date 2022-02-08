56b9897aee53342044d570ca14fa1e7dc6e7bb8b
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-02-07 19:41:22 -0600

    exported a get method for customers by id

--- src/modules/EmployeeManager.js
+++ src/modules/EmployeeManager.js
@@ -1,5 +1,9 @@
 const remoteURL = 'http://localhost:8088'
 
+export const getCustomerById = (employeeId) => {
+    return fetch(`${remoteURL}/customers/${employeeId}?_expand=locationId`).then(res => res.json())
+}
+
 export const getAllEmployees = () => {
     return fetch(`${remoteURL}/employees`).then(res => res.json())
 } 
