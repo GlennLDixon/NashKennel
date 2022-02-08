377f8e4f2edf481a2c3880512a8cb22cce1e08b6
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-02-07 19:42:58 -0600

    removed the id from the expand paths for the getAnimalById method

--- src/modules/AnimalManager.js
+++ src/modules/AnimalManager.js
@@ -1,11 +1,13 @@
 const remoteURL = 'http://localhost:8088'
 
 export const getAnimalById = (animalId) => {
-    return fetch(`${remoteURL}/animals/${animalId}?_expand=locationId&_expand=customerId`).then(res => res.json())
+    return fetch(`${remoteURL}/animals/${animalId}?_expand=location&_expand=customer`)
+    .then(res => res.json())
 }
 
 export const getAllAnimals = () => {
-    return fetch(`${remoteURL}/animals`).then(res => res.json())
+    return fetch(`${remoteURL}/animals`)
+    .then(res => res.json())
 } 
 
 export const deleteAnimal = (id) => {
