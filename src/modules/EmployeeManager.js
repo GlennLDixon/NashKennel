297ac9ae7d960b4518cb74d9f22854e66408e7c1
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-02-12 09:11:21 -0600

    corrected export name

--- src/modules/EmployeeManager.js
+++ src/modules/EmployeeManager.js
@@ -1,7 +1,7 @@
 const remoteURL = 'http://localhost:8088'
 
-export const getCustomerById = (employeeId) => {
-    return fetch(`${remoteURL}/customers/${employeeId}?_expand=locationId`).then(res => res.json())
+export const getEmployeeById = (employeeId) => {
+    return fetch(`${remoteURL}/employees/${employeeId}?_expand=locationId`).then(res => res.json())
 }
 
 export const getAllEmployees = () => {
