c35d546155a7a69eec9fd159f98deab008b33561
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-02-14 18:52:30 -0600

    created a submit button that will navigate to the submit form

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react"
 import { AnimalCard } from './AnimalCard.js'
 import { getAllAnimals, getAnimalById, deleteAnimal } from "../../modules/AnimalManager.js"
+import { useNavigate } from "react-router-dom"
 
 export const AnimalList = () => {
     const [animals, setAnimals] = useState([])
@@ -16,19 +17,30 @@ export const AnimalList = () => {
         .then(() => getAllAnimals().then(setAnimals));
     };
 
+    const navigate = useNavigate()
 
     useEffect(() => {
         getAnimals();
     }, []);
 
+
     return(
-        <div className="container-cards">
-            {animals.map(animal => 
-                <AnimalCard 
-                key={animal.id} 
-                animal={animal} 
-                handleDeleteAnimal={handleDeleteAnimal} />)}
-        </div>
+        <>
+            <section className="section-content">
+                <button type="button"
+                    className="btn"
+                    onClick={() => {navigate("/animals/create")}}>
+                    Admit Animal
+                </button>
+            </section>
+            <div className="container-cards">
+                {animals.map(animal => 
+                    <AnimalCard 
+                    key={animal.id} 
+                    animal={animal} 
+                    handleDeleteAnimal={handleDeleteAnimal} />)}
+            </div>
+        </>
     )
 
 }
